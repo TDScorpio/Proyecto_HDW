ea94a967e608745372985ce2e26026d5bf2c0248
Luciano <lucianopizarro806@gmail.com>
2025-09-18 00:07:26 -0500

    Limpiando main, dejando solo archivos principales

--- script.js
+++
@@ -1,5 +0,0 @@
-// Menú hamburguesa responsive
-document.getElementById('hamburger-btn').addEventListener('click', function() {
-  const nav = document.querySelector('.nav-links');
-  nav.classList.toggle('open');
-});
